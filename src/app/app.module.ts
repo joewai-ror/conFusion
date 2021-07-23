2b255da4ba7d3b52f29685928f40573ac5d2140d
Joe <joe.leong@binfinite.com.my>
2021-07-23 22:04:19 +0800

    Exceise 1 correction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
